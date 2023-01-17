41b19013b8b37eed9e51eac5af27cf2e8a82ae6d
jterry@greenspacemeetings.com <jterry@greenspacemeetings.com>
2023-01-16 21:20:41 -0500

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "greenspace-meeting--38323",
   url: "https://greenspace-meeting--38323.botics.co",
   api: "https://greenspace-meeting--38323.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
