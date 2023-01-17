faf8177662a2053a9c8fab241f95a91ab0b82fba
Crowdbotics <team@crowdbotics.com>
2023-01-17 01:38:57 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/greenspace_meeting__38323/MainActivity.java
@@ -0,0 +1,15 @@
+package com.greenspace_meeting__38323;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "greenspace_meeting__38323";
+  }
+}
